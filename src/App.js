c814948c3ee2abb3d18eec1b523734595ee5bbfb
Aditya Dhanraj <opsingh861@gmail.com>
2023-11-17 09:18:25 +0530

    added some file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div style={{height:'90vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+      <h1>Hello DevOps!</h1>
+    </div>
+  );
+}
+
+export default App;
